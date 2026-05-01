16294cfaa1936fa56d01faf0fd7bb259a9f74a37
Wesley Elliott <wesserbear@gmail.com>
2026-05-01 02:17:49 -0600

    Main Page Outline and design
    Added a bar at the top which I want to be customizable eventually. but its a good start. I added all the colors that I wanted in class AppColors with the correct hex codes. I also added the bottom navigation bar with default icons which I kind of like.

--- book_club_tracking_app/lib/main.dart
+++ book_club_tracking_app/lib/main.dart
@@ -1,122 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+class AppColors {
+  // Backgrounds
+  static const background =Color(0xFF1B1410);
+  static const card =Color(0xFF241B16);
+  static const secondary =Color(0xFF2E2218);
+  static const muted =Color(0xFF2A1F18);
+
+  // Text / Foregrounds
+  static const foreground =Color(0xFFF1E5CF);
+  static const primaryForeground =Color(0xFF1B1410);
+  static const secondaryFg =Color(0xFFE8DBC2);
+  static const mutedForeground =Color(0xFF9A8773);
+  static const accentForeground =Color(0xFFF1E5CF);
+
+  // Accents / Actions
+  static const primary =Color(0xFFD6A85F); // gold
+  static const accent =Color(0xFFA45D3F);
+  static const destructive =Color(0xFFC75A47);
+
+  // Border
+  static const border =Color(0xFF3A2C22);
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: Scaffold(
+        backgroundColor: AppColors.background,
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+        appBar: AppBar(
+          backgroundColor: AppColors.background,
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Book Talk",
+                  style: GoogleFonts.lora(
+                    color: AppColors.foreground,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "stupid ahh bookclub",
+                  style: GoogleFonts.inter(
+                    color: AppColors.mutedForeground,
+                    fontSize: 15,
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
+
+          bottom: PreferredSize(preferredSize: const Size.fromHeight(12), child: Container(height: 1, color: AppColors.border,))
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+
+
+
+
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(color: AppColors.border, width: 1)
+            )
+          ),
+        
+          child : BottomNavigationBar(
+            
+            backgroundColor: AppColors.background,
+            currentIndex: 1,
+            selectedItemColor: AppColors.primary,
+            unselectedItemColor: AppColors.mutedForeground,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.people_outlined),
+                label: 'Members',
+                ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book_outlined),
+                label: 'Library',
+                ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_month_outlined),
+                label: 'Meetings',
+                ),
+            ]
+          ),
+        )
+      )
     );
   }
 }
